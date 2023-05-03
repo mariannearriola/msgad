updated with backprop. bug fixes

msgad/label_generation.py
@@ -36,21 +36,73 @@ class LabelGenerator:
             anom_flat=np.concatenate((anom_flat,i))
         return anom_flat
 
-    def filter_anoms(self,labels,anoms,vis_name,img_num=None):
-        np.random.seed(1)
-        #print('filter anoms',torch.cuda.memory_allocated()/torch.cuda.max_memory_reserved())
-        signal = np.random.randn(self.feats.shape[0],self.feats.shape[-1])/2
-        #signal = np.ones((labels[0].shape[0],self.feats.shape[-1]))
-        
-        #anom_tot = 0
-        #for anom in anoms[:-1]:
-        #    anom_tot += self.flatten_label(anom).shape[0]
-        #anom_tot += anoms[-1].shape[0]
+    def anom_response(self,adj,labels,anoms,vis_name,img_num=None):
+        signal = np.random.randn(self.feats.shape[0],self.feats.shape[0])
+
+        anom_tot = 0
+        for anom in list(anoms.values())[:-1]:
+            anom_tot += self.flatten_label(anom).shape[0]
+        anom_tot += list(anoms.values())[-1].shape[0]
         all_nodes = torch.arange(self.feats.shape[0])
+        e,U = self.get_spectrum(adj.to(torch.float64))
+        es,us=[e],[U]
+        del e, U ; torch.cuda.empty_cache() ; gc.collect()
+        for i,label in enumerate(labels):
+            lbl=torch.maximum(label, label.T)
+            e,U = self.get_spectrum(lbl.to(torch.float64))
+            es.append(e) ; us.append(U)
+            del lbl, e, U ; torch.cuda.empty_cache() ; gc.collect()
+
+        for anom_ind,anom in enumerate(anoms.values()):
+            plt.figure()
+            legend = []
+            for i,label in enumerate(range(len(es))):
+                e,U = es[i],us[i]
+                e = e.to(self.graph.device) ; U = U.to(self.graph.device)
+
+                #anom = anom.flatten()
+                if len(anom) == 0:
+                    continue
+                if anom_ind != len(anoms)-1:
+                    anom_f = self.flatten_label(anom)
+                else:
+                    anom_f = anom
+                #anom_mask=np.setdiff1d(all_nodes,anom_f)
+                signal_ = copy.deepcopy(signal)+1
+                signal_[anom_f]=(np.random.randn(U.shape[0])*400*anom_tot/anom.shape[0])+1# NOTE: tried 10#*(anom_tot/anom.shape[0]))
+                
+                self.plot_spectrum(e.detach().cpu(),U.detach().cpu(),signal_)
+                if i == 0:
+                    legend.append('original adj')
+                else:
+                    legend.append(f'{i} label')
+            plt.legend(legend)
+
+            fpath = f'vis/filter_anom_ev/{self.dataset}/{self.model_str}/{self.label_type}/{self.epoch}'
+            if not os.path.exists(fpath):
+                os.makedirs(fpath)
+            plt.savefig(f'{fpath}/filter_vis_{self.label_type}_{vis_name}_filter{list(anoms.keys())[anom_ind]}.png')
+            del e,U ; torch.cuda.empty_cache() ; gc.collect()
+                
+            torch.cuda.empty_cache()
+
+    def filter_anoms(self,labels,anoms,vis_name,img_num=None):
+        np.random.seed(123)
+        #print('filter anoms',torch.cuda.memory_allocated()/torch.cuda.max_memory_reserved())
+        signal = np.random.randn(self.feats.shape[0],self.feats.shape[0])
+
+        #signal = np.ones((labels[0].shape[0],self.feats.shape[-1]))
+
+        anom_tot = 0
+        for anom in list(anoms.values())[:-1]:
+            anom_tot += self.flatten_label(anom).shape[0]
+        anom_tot += list(anoms.values())[-1].shape[0]
+        all_nodes = torch.arange(self.feats.shape[0])
+
         for i,label in enumerate(labels):
             lbl=torch.maximum(label, label.T)
             try:
-                e,U = self.get_spectrum(lbl)
+                e,U = self.get_spectrum(lbl.to(torch.float64))
             except Exception as e:
                 print(e)
                 import ipdb ; ipdb.set_trace()
@@ -66,7 +118,6 @@ class LabelGenerator:
                 import ipdb ; ipdb.set_trace()
             legend_arr = ['no anom signal','sc1 anom signal','sc2 anom signal','sc3 anom signal','single anom signal']
             legend =['no anom signal']
-            
             for anom_ind,anom in enumerate(anoms.values()):
                 #anom = anom.flatten()
                 if len(anom) == 0:
@@ -76,13 +127,13 @@ class LabelGenerator:
                 else:
                     anom_f = anom
                 #anom_mask=np.setdiff1d(all_nodes,anom_f)
-                signal_ = np.copy(signal)
+                signal_ = copy.deepcopy(signal)+1
                 try:
-                    signal_[anom_f]*=400# NOTE: tried 10#*(anom_tot/anom.shape[0]))
+                    signal_[anom_f]=(np.random.randn(U.shape[0])*400*anom_tot/anom.shape[0])+1# NOTE: tried 10#*(anom_tot/anom.shape[0]))
                 except Exception as e:
                     print(e)
                     import ipdb ; ipdb.set_trace()
-                signal_ += 1
+                
                 self.plot_spectrum(e.detach().cpu(),U.detach().cpu(),signal_)
                 legend.append(legend_arr[anom_ind+1])
                 plt.legend(legend)
@@ -141,7 +192,6 @@ class LabelGenerator:
         return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
 
     def get_spectrum(self,mat):
-        
         d_ = np.zeros(mat.shape[0])
         degree_in = np.ravel(mat.sum(axis=0).detach().cpu().numpy())
         degree_out = np.ravel(mat.sum(axis=1).detach().cpu().numpy())
@@ -149,10 +199,13 @@ class LabelGenerator:
         disconnected = (dw == 0)
         np.power(dw, -0.5, where=~disconnected, out=d_)
         D = scipy.sparse.diags(d_)
-        L = torch.eye(mat.shape[0])
-        L -= torch.tensor(D * mat.detach().cpu().numpy() * D)
+        #L = torch.eye(mat.shape[0])
+        mat_sparse = scipy.sparse.csr_matrix(mat.detach().cpu().numpy())
+        L = scipy.sparse.identity(mat.shape[0]) - D * mat_sparse * D
+        #L -= torch.tensor(D * mat.detach().cpu().numpy() * D)
         L[disconnected, disconnected] = 0
-        L = L.to(mat.device)
+        L.eliminate_zeros()
+        #L = L.to(mat.device)
         '''
         edges = torch.vstack((mat.edges()[0],mat.edges()[1])).T
         L = torch_geometric.utils.get_laplacian(edges.T,torch.sigmoid(mat.edata['w']),normalization='sym',num_nodes=self.feats.shape[0])
@@ -160,11 +213,17 @@ class LabelGenerator:
         #print('eig', torch.cuda.memory_allocated()/torch.cuda.max_memory_reserved())
         '''
         try:
-            e,U = torch.linalg.eigh(L)
+            e,U = torch.linalg.eigh(torch.tensor(L.toarray()).to(mat.device))
+            #e,U=scipy.linalg.eigh(np.asfortranarray(L.toarray()), overwrite_a=True)
+            #e,U=scipy.linalg.eigh(np.asfortranarray(L.detach().cpu().numpy()), overwrite_a=True)
+            assert -1e-5 < e[0] < 1e-5
+            e[0] = 0
         except Exception as e:
             print(e)
             return
+        e,U = torch.tensor(e).to(mat.device),torch.tensor(U).to(mat.device)
         #del L, edges ; torch.cuda.empty_cache() ; gc.collect()
+        #import ipdb ; ipdb.set_trace()
         del L, D ; torch.cuda.empty_cache() ; gc.collect()
         #print('eig done', torch.cuda.memory_allocated()/torch.cuda.max_memory_reserved())
         '''
@@ -175,16 +234,20 @@ class LabelGenerator:
             import ipdb ; ipdb.set_trace()
             e[0] = 0
         '''
-        return e, U.to(torch.float32)
+        return e, U#.to(torch.float32)
 
     def plot_spectrum(self,e,U,signal,color=None):
+        #import ipdb ; ipdb.set_trace()
         c = U.T@signal
-        M = torch.zeros((21,c.shape[1])).to(e.device)
-        for j in range(e.shape[0]):
-            idx = min(int(e[j] / 0.1), 21-1)
+        M = torch.zeros((10,c.shape[1])).to(e.device).to(U.dtype)
+        for j in range(c.shape[0]):
+            idx = min(int(e[j] / 0.1), 10-1)
             M[idx] += c[j]**2
         M=M/sum(M)
-        y = M[:,0].detach().cpu().numpy()
+        #y = M[:,0].detach().cpu().numpy()
+        #print('nans',torch.where(torch.isnan(M))[0].shape)
+        M[torch.where(torch.isnan(M))]=0
+        y = torch.mean(M,axis=1).detach().cpu().numpy()
         x = np.arange(y.shape[0])
         try:
             spline = make_interp_spline(x, y)
@@ -303,9 +366,12 @@ class LabelGenerator:
                 coeffs =  self.get_bern_coeff(K)
                 label_idx=np.arange(K+1)
                 if 'cora' in self.dataset:
+                    label_idx = [0,2,5]
+                #    label_idx=[0,2,4]
+                #    label_idx = [1,2,10]
                 #    label_idx = [1,3]
                 #    label_idx = [0,9]
-                    label_idx = [2,5]
+                #    label_idx = [2,5]
                 #    label_idx = np.add(label_idx,1)
                 if 'elliptic' in self.dataset and self.batch_size != 0:
                     label_idx = [6,8,10]
@@ -314,12 +380,12 @@ class LabelGenerator:
             elif 'bwgnn' in self.label_type:
                 coeffs = self.calculate_theta2(K)
                 label_idx=np.arange(K+1)
-                if 'cora' in self.dataset:
-                    label_idx = [1,5]
+                #if 'cora' in self.dataset:
+                #    label_idx = [1,5]
             labels = []
             adj_label = None
 
-            dgl.seed(1)
+            dgl.seed(123)
             g = self.graph.to('cpu')#self.graph.device)#.to('cpu')
             if 'cora' in self.dataset: g = g.to(self.graph.device)
             g = dgl.to_homogeneous(g).subgraph(g.srcnodes())
@@ -329,14 +395,14 @@ class LabelGenerator:
             else:
                 g.edata['w'] = self.norm(g,torch.ones(num_a_edges).to('cpu')).to('cpu')#(self.graph.device)).to(self.graph.device)
             adj_label = g.adjacency_matrix().to_dense()
-            if self.vis == True and 'test' not in self.vis_name:
-                self.filter_anoms([adj_label.to(self.graph.device)],self.anoms,self.vis_name,'og')
+            #if self.vis == True and 'test' not in self.vis_name:
+            self.filter_anoms([adj_label.to(self.graph.device)],self.anoms,self.vis_name,'og')
             
             if 'cora' not in self.dataset: print('num a edges',num_a_edges)
         
             # NOTE : remove if og wanted
-            if 'cora' in self.dataset or 'weibo' in self.dataset:
-                labels = [adj_label.to(self.graph.device)]
+            #if 'cora' in self.dataset:# or 'weibo' in self.dataset:
+            #    labels = [adj_label.to(self.graph.device)]
             if adj_label is not None: del adj_label ; torch.cuda.empty_cache()
             
             prods = [g]
@@ -390,6 +456,7 @@ class LabelGenerator:
                     #if 'cora' not in self.dataset: print(basis.number_of_edges())
                     basis_ = basis.adjacency_matrix().to_dense().to(self.graph.device)
                     #basis_[basis.edges()] = basis.edata['w'].to(self.graph.device)
+                    
                     #basis_ = torch.nn.functional.pad(basis_,(0,zero_rows,0,zero_rows))
                     # TO ADD:  basis_.fill_diagonal_(1.)
                     #del nz, sorted_idx
@@ -413,54 +480,53 @@ class LabelGenerator:
             del g ; torch.cuda.empty_cache()
             #print('done')
             #print("Seconds to get labels", (time.time()-seconds)/60)
-            
+
         if self.vis == True and 'test' not in self.vis_name:
-            if self.graph.device == 'cpu':
+            if self.graph.device == 'cpu': 
                 print('visualizing labels')
             
             plt.figure()
-            #import ipdb ; ipdb.set_trace()
             adj_label = dgl.to_homogeneous(self.graph).subgraph(self.graph.srcnodes()).adjacency_matrix().to_dense()
             adj_label=np.maximum(adj_label, adj_label.T).to(self.graph.device) #?
             
-            legend = []
+            e_adj,U_adj = self.get_spectrum(torch.tensor(adj_label).to(self.graph.device).to(torch.float64))
+            self.plot_spectrum(e_adj,U_adj,self.feats.to(U_adj.dtype),color="cyan")
+            fpath = f'vis/label_vis/{self.dataset}/{self.model_str}/{self.label_type}'
+            if not os.path.exists(fpath):
+                os.makedirs(fpath)
+            plt.savefig(f'{fpath}/labels_{self.label_type}_{self.epoch}_{self.vis_name}_og.png')
+            del e_adj, U_adj ; torch.cuda.empty_cache() ; gc.collect()
+            
+            legend = ['og']
             for label_ind,label in enumerate(labels):
                 #print(label_ind)
                 label = label
                 if not torch.equal(label,label.T):
                     label=torch.maximum(label, label.T).to(self.graph.device) 
                 try:
-                    e,U = self.get_spectrum(label)
+                    e,U = self.get_spectrum(label.to(torch.float64).to(self.graph.device))
                     e = e.to(self.graph.device)
                     U = U.to(self.graph.device)
                 except Exception as e:
                     print(e)
                     import ipdb ; ipdb.set_trace()
                 try:
-                    self.plot_spectrum(e,U,self.feats)
+                    self.plot_spectrum(e,U,self.feats.to(U.dtype))
                 except Exception as e:
                     print(e)
                     import ipdb ; ipdb.set_trace()
                 del e,U,label ; torch.cuda.empty_cache() ; gc.collect()
-                fpath = f'vis/label_vis/{self.dataset}/{self.model_str}/{self.label_type}'
                 if not os.path.exists(fpath):
                     os.makedirs(fpath)
-                plt.savefig(f'{fpath}/labels_{self.label_type}_{self.epoch}_{self.vis_name}_{label_ind+1}.png')
-                #legend.append(f'label {str(label_ind+1)}')
-
-            #legend.append('original adj.')
-            e_adj,U_adj = self.get_spectrum(torch.tensor(adj_label).to(self.graph.device))
-            self.plot_spectrum(e_adj, U_adj,self.feats,color="cyan")
-            plt.savefig(f'{fpath}/labels_{self.label_type}_{self.epoch}_{self.vis_name}_og.png')
-            del e_adj, U_adj, adj_label ; torch.cuda.empty_cache() ; gc.collect()
+                #plt.savefig(f'{fpath}/labels_{self.label_type}_{self.epoch}_{self.vis_name}_{label_ind+1}.png')
+                legend.append(f'label {str(label_ind+1)}')
+            plt.legend(legend)
+            plt.savefig(f'{fpath}/labels_{self.label_type}_{self.epoch}_{self.vis_name}_{label_ind+1}.png')
 
             #plt.legend(legend)
-            fpath = f'vis/label_vis/{self.dataset}/{self.model_str}/{self.label_type}'
-            if not os.path.exists(fpath):
-                os.makedirs(fpath)
             #plt.savefig(f'{fpath}/labels_{self.label_type}_{self.epoch}_{self.vis_name}.png')
             #print("Seconds to visualize labels", (time.time()-seconds)/60)
-        
+            self.anom_response(adj_label.to(self.graph.device),labels,self.anoms,self.vis_name)
+            del adj_label ; torch.cuda.empty_cache() ; gc.collect()
             #self.filter_anoms(labels,self.anoms,self.vis_name)
-
         return labels

msgad/loss_utils.py
@@ -41,7 +41,7 @@ def loss_func(graph, feat, A_hat, X_hat, pos_edges, neg_edges, sample=False, rec
             feat = graph.ndata['feature']
             edge_labels = torch.cat((torch.full((pos_edges.shape[0],),1.),(torch.full((neg_edges.shape[0],),0.))))
             edge_labels = edge_labels.to(graph.device)
-    
+
     for recons_ind,preds in enumerate([A_hat, X_hat]):
         if preds == None: continue
         for ind, sc_pred in enumerate(preds):
@@ -129,8 +129,8 @@ def get_sampled_losses(pred,edges,label):
     label = label[edges[:,0],edges[:,1]]
     #return torch.nn.functional.binary_cross_entropy_with_logits(edge_errors,label), torch.nn.functional.binary_cross_entropy_with_logits(edge_errors,label,reduction='none')
 
-    #edge_errors = torch.pow(torch.abs(edge_errors-label),2)
-    edge_errors = torch.abs(edge_errors-label)
+    edge_errors = torch.pow(torch.abs(edge_errors-label),2)
+    #edge_errors = torch.abs(edge_errors-label)
     #total_error = torch.mean(torch.sqrt(edge_errors))
-    total_error = torch.mean(edge_errors)
+    total_error = torch.sum(edge_errors)
     return total_error, edge_errors

msgad/model.py
@@ -71,8 +71,8 @@ class GraphReconstruction(nn.Module):
         """Obtain graph information from input TODO: MOVE?"""
         edges = torch.vstack((graph.edges()[0],graph.edges()[1]))
         feats = graph.ndata['feature']
-        if 'edge' == self.batch_type:
-            feats = feats['_N']
+        #if 'edge' == self.batch_type:
+        #    feats = feats['_N']
         return edges, feats, graph
 
     def stationary_distribution(self, M, device):
@@ -133,12 +133,12 @@ class GraphReconstruction(nn.Module):
                     oom = False
                     #mem = torch.cuda.memory_allocated()/torch.cuda.max_memory_reserved()
                     #print(mem)
-                    try:
-                        recons,res,attn_scores = i(feats,edges,dst_nodes)
-                        #recons,res,attn_scores = i(feats,graph.adjacency_matrix().to_dense().to(graph.device),dst_nodes)
-                    except Exception as e:
-                        print(e)
-                        oom = True
+                    #try:
+                    recons,res,attn_scores = i(feats,edges,dst_nodes)
+                    #recons,res,attn_scores = i(feats,graph.adjacency_matrix().to_dense().to(graph.device),dst_nodes)
+                    #except Exception as e:
+                    #    print(e)
+                    #    oom = True
                     if oom:
                         print('oom')
                         import ipdb ; ipdb.set_trace()

msgad/models/amnet_ms.py
@@ -101,25 +101,25 @@ class BernConv(MessagePassing):
             Bx_next = self.propagate(edge_index, x=Bx_next, norm=norm, size=None)
             #Bx_next = mat @ Bx_next
             Bx.append(Bx_next)
-
         bern_coeff =  BernConv.get_bern_coeff(self.K)
         eps = 1e-2
-        if self.normalization:
-            weight = torch.sigmoid(self.weight)
-        else:
-            weight = torch.clamp(self.weight, min = 0. + eps, max = 1. - eps)
-
+        #if self.normalization:
+        #    weight = torch.sigmoid(self.weight)
+        #else:
+        #    weight = torch.clamp(self.weight, min = 0. + eps, max = 1. - eps)
+        #print(weight)
         out = torch.zeros_like(x)
         for k in range(0, self.K + 1):
             coeff = bern_coeff[k]
             basis = Bx[0] * coeff[0]
             for i in range(1, self.K + 1):
                 basis += Bx[i] * coeff[i]
-            out += basis * weight[k]
-
+            out += basis * F.softmax(self.weight)[k]
+            
+        # NOTE: out is just epsilon (need to add weights to paramter list of amnet ms)
         del lambda_max
         del basis
-        del weight
+        #del weight
         del Bx
         del bern_coeff
         del Bx_next
@@ -127,7 +127,6 @@ class BernConv(MessagePassing):
         #del edge_index
         #del norm
         torch.cuda.empty_cache()
-        
         return out
 
     
@@ -176,8 +175,10 @@ class AMNet_ms(nn.Module):
                                                  )
         self.K = K
         self.filters = nn.ModuleList([BernConv(hid_channels, K, normalization=False, bias=True) for _ in range(filter_num)])
+        #self.filters = nn.ModuleList([BernConv(hid_channels, K, normalization=False, bias=True),BernConv(hid_channels, K, normalization=False, bias=True),BernConv(hid_channels, K, normalization=False, bias=True),BernConv(hid_channels, K, normalization=False, bias=True),BernConv(hid_channels, K, normalization=False, bias=True)])
+        #self.filters.extend([BernConv(hid_channels, K, normalization=False, bias=True) for i in range(1, filter_num)])
         self.filter_num = filter_num
-        
+
         self.W_f = nn.Sequential(nn.Linear(hid_channels, in_channels),
                                  self.attn_fn,
                                  )
@@ -186,7 +187,7 @@ class AMNet_ms(nn.Module):
                                  self.attn_fn,
                                  )
         
-        self.out_l = nn.Linear(hid_channels, hid_channels)
+        #self.out_l = nn.Linear(hid_channels, hid_channels)
         #self.lam = nn.Parameter(data=torch.normal(mean=torch.full((filter_num,),0.),std=1))
         '''
         self.linear_cls_out = nn.Sequential(
@@ -200,7 +201,6 @@ class AMNet_ms(nn.Module):
         self.relu = torch.nn.ReLU()
         '''
         self.lam = nn.Parameter(data=torch.normal(mean=torch.full((filter_num,),0.),std=1))
-
         self.reset_parameters()
 
 
@@ -232,19 +232,23 @@ class AMNet_ms(nn.Module):
 
         x = self.linear_transform_in(x)
         h_list = []
+        lams = F.softmax(self.lam)
         for i, filter_ in enumerate(self.filters):
-            h = filter_(x, edge_index)
-            h_list.append(h * torch.sigmoid(self.lam[i]))
+            #print('for filter',i,self.filters[i].weight)
+            #h = filter_(x, edge_index)
+            h = self.filters[i](x, edge_index)*lams[i]
+            h_list.append(h)
             del filter_
             del h
-        
+        print(lams)
         torch.cuda.empty_cache()
         h_filters = torch.stack(h_list, dim=1)
         h_filters_proj = self.W_f(h_filters)
         x_proj = self.W_x(x).unsqueeze(-1)
         score_logit = torch.bmm(h_filters_proj, x_proj)
-        #soft_score = F.softmax(score_logit, dim=1) ; score = soft_score
-        score = score_logit # shape: [ num_nodes, K, num_filters ] ; node-wise attention
+        soft_score = F.softmax(score_logit, dim=1) ; score = soft_score
+
+        #score = score_logit # shape: [ num_nodes, K, num_filters ] ; node-wise attention
         # attention for various freq. profiles
         res = h_filters[:, 0, :] * score[:, 0]
         for i in range(1, self.filter_num):
@@ -253,7 +257,7 @@ class AMNet_ms(nn.Module):
             import ipdb ; ipdb.set_trace()
             print('nan')
             
-        res = self.out_l(res)
+        #res = self.out_l(res)
         edge_index, norm = self.__norm__(edge_index, x.shape[0],
                                          None, 'sym', torch.tensor(2.0, dtype=x.dtype, device=x.device), dtype=x.dtype)
 
@@ -262,13 +266,13 @@ class AMNet_ms(nn.Module):
         #L[edge_index[0],edge_index[1]]=norm
         #self.L = L
         #res_ = torch.sigmoid(res@res.T)
-        
+        #import ipdb ; ipdb.set_trace()
         res_ = res@res.T
         del h_filters
         del x
-        #del score_logit
+        del score_logit
         del x_proj
-        #del h_filters_proj
+        del h_filters_proj
         for i in range(len(h_list)):
             del h_list[0]
         torch.cuda.empty_cache()

msgad/run.py
@@ -9,6 +9,7 @@ import time
 import gc
 from models.gcad import *
 from model import *
+import torch.nn.functional as F
 import MADAN.Madan as md
 from visualization import *
 
@@ -18,13 +19,14 @@ warnings.filterwarnings("ignore")
 def graph_anomaly_detection(args):
     """Pipeline for autoencoder-based graph anomaly detection"""
     # load data
-    torch.manual_seed(1) ; dgl.seed(1) ; np.random.seed(1)
+    torch.manual_seed(123) ; dgl.seed(123) ; np.random.seed(123)
     sp_adj, edge_idx, feats, truth, sc_label = load_anomaly_detection_dataset(args.dataset, args.scales)
     
     anoms,norms=np.where(truth==1)[0],np.where(truth==0)[0]
     #import ipdb ; ipdb.set_trace()
     if sp_adj is not None:
         adj = sparse_matrix_to_tensor(sp_adj,feats)
+
     lbl,last_batch_node,pos_edges,neg_edges=None,None,None,None
     
     # initialize data loading
@@ -79,11 +81,19 @@ def graph_anomaly_detection(args):
                 neg_edges = sub_graph_neg.edges()
             else:
                 in_nodes, pos_edges, neg_edges, g_batch, last_batch_node = get_edge_batch(loaded_input)
+      
                 #g_batch.add_edges(pos_edges[:,0],pos_edges[:,1])
                 if args.datasave:
                     g_batch = g_batch[0]
+                # NOTE: EDIT: batched adjacency matrix is shuffled
+                feat = g_batch.ndata['feature']
+                g_adj = g_batch.adjacency_matrix().to_dense()[torch.argsort(in_nodes)][:,torch.argsort(in_nodes)]
+                src,dst=g_adj.nonzero()[:,0],g_adj.nonzero()[:,1]
+                g_batch = dgl.graph((src,dst)).to(g_batch.device)
+                g_batch.ndata['feature']=feat['_N'].to(g_batch.device)
+                
                 if 'cora' not in args.dataset: print('size of batch',g_batch.num_dst_nodes(),'nodes')
-            
+
             if args.model == 'madan':
                 if args.debug:
                     if iter % 100 == 0:
@@ -142,6 +152,17 @@ def graph_anomaly_detection(args):
                 A_hat,X_hat,model_lbl,res_a = struct_model(g_batch,last_batch_node,pos_edges,neg_edges,batch_sc_label,vis=vis,vis_name='epoch1')
                 if args.datasave:
                     lbl = model_lbl
+
+                diffs = []
+                ranges =[]
+                for ind_,A_ in enumerate(A_hat):
+                    ranges.append(A_.max()-A_.min())
+                    if ind_ == len(A_hat)-1:
+                        break
+                    diffs.append((A_-A_hat[ind_+1]).max())
+
+                print("diffs",diffs)
+                print("ranges",ranges)
                     
                     
             if args.batch_type == 'node':
@@ -159,7 +180,8 @@ def graph_anomaly_detection(args):
                         del l ; torch.cuda.empty_cache()
                     recons_label = lbl_
                     del lbl_ ; torch.cuda.empty_cache()
-                loss, struct_loss, feat_cost = loss_func(recons_label, g_batch.ndata['feature']['_N'], A_hat, X_hat, pos_edges, neg_edges, sample=args.sample_test, recons=args.recons,alpha=args.alpha)
+                loss, struct_loss, feat_cost = loss_func(recons_label, g_batch.ndata['feature'], A_hat, X_hat, pos_edges, neg_edges, sample=args.sample_test, recons=args.recons,alpha=args.alpha)
+                #loss, struct_loss, feat_cost = loss_func(recons_label, g_batch.ndata['feature']['_N'], A_hat, X_hat, pos_edges, neg_edges, sample=args.sample_test, recons=args.recons,alpha=args.alpha)
             
             if 'multi-scale' in args.model:
                 #l = torch.sum(torch.mean(loss))
@@ -264,9 +286,15 @@ def graph_anomaly_detection(args):
         else:
             in_nodes, pos_edges, neg_edges, g_batch, last_batch_node = get_edge_batch(loaded_input)
             edge_ids = torch.vstack((pos_edges,neg_edges))
-            if args.datasave:# or 'elliptic' in args.model:
-                g_batch = g_batch[0]
             #g_batch.add_edges(pos_edges[:,0],pos_edges[:,1])
+            if args.datasave:
+                g_batch = g_batch[0]
+            # NOTE: EDIT: batched adjacency matrix is shuffled
+            feat = g_batch.ndata['feature']
+            g_adj = g_batch.adjacency_matrix().to_dense()[torch.argsort(in_nodes)][:,torch.argsort(in_nodes)]
+            src,dst=g_adj.nonzero()[:,0],g_adj.nonzero()[:,1]
+            g_batch = dgl.graph((src,dst)).to(g_batch.device)
+            g_batch.ndata['feature']=feat['_N'].to(g_batch.device)
     
         # run evaluation
         if struct_model and args.model != 'gcad':
@@ -308,7 +336,8 @@ def graph_anomaly_detection(args):
                     del l ; torch.cuda.empty_cache()
                 recons_label = lbl_
                 del lbl_ ; torch.cuda.empty_cache()
-            loss, struct_loss, feat_cost = loss_func(recons_label, g_batch.ndata['feature']['_N'], A_hat, X_hat, pos_edges, neg_edges, sample=args.sample_test, recons=args.recons,alpha=args.alpha)
+            #loss, struct_loss, feat_cost = loss_func(recons_label, g_batch.ndata['feature']['_N'], A_hat, X_hat, pos_edges, neg_edges, sample=args.sample_test, recons=args.recons,alpha=args.alpha)
+            loss, struct_loss, feat_cost = loss_func(recons_label, g_batch.ndata['feature'], A_hat, X_hat, pos_edges, neg_edges, sample=args.sample_test, recons=args.recons,alpha=args.alpha)
         
         if args.model in ['gradate']:
             loss = A_hat[0]
@@ -381,17 +410,25 @@ def graph_anomaly_detection(args):
             else:
                 a_clf.calc_prec(adj.adjacency_matrix().to_dense(), edge_anom_mats, truth, sc_label, args, cluster=False)
     if args.vis_filters == True:
-        visualizer = Visualizer(adj,feats,args,sc_label,norms,anoms)
-        try:
-            visualizer.plot_recons(recons_a)
-            visualizer.plot_filters(res_a_all)
-        except Exception as e:
-            print(e)
+        visualizer = Visualizer(adj,feats,args,sc_label,norms,anoms,recons_label)
+        visualizer.plot_recons(recons_a)
+        visualizer.plot_filters(res_a_all)
         if 'multi-scale-amnet' == args.model:
-            visualizer.plot_attn_scores(train_attn_w.numpy())
+            visualizer.plot_attn_scores(train_attn_w.numpy(),[F.softmax(i.lam).detach().cpu().numpy() for i in struct_model.module_list])
 
-    for i in struct_model.module_list:
-        print(torch.sigmoid(i.lam))
+    plt.figure()
+    legend = []
+    width,offset = 0.25,0
+    for ind,i in enumerate(struct_model.module_list):
+        plt.bar(np.arange(len(i.lam))+offset,F.softmax(i.lam).detach().cpu().numpy(),width=width)
+        offset += width
+        legend.append(ind)
+    plt.legend(legend)
+    fpath = f'vis/lam_vis/{args.dataset}/{args.model}/{args.label_type}'
+    if not os.path.exists(fpath):
+        os.makedirs(fpath)
+    plt.savefig(f'{fpath}/{args.epoch}_epoch.png')
+        
 
         
 

msgad/utils.py
@@ -13,6 +13,7 @@ import networkx as nx
 from igraph import Graph
 import dgl
 import copy
+import matplotlib.ticker as mtick
 import pickle as pkl
 from model import GraphReconstruction
 from models.gcad import *
@@ -27,9 +28,12 @@ def get_batch_sc_label(in_nodes,sc_label,g_batch,node_dict):
         sc_labels = []
         if len(sc_) == 0: sc_labels.append([])
         for sc__ in sc_:
+            sc_labels.append(sc__)
+            continue
             if np.intersect1d(in_nodes[g_batch.dstnodes().detach().cpu().numpy()],sc__).shape[0]>0:
                 sc_labels.append(np.vectorize(node_dict.get)(np.intersect1d(in_nodes[g_batch.dstnodes().detach().cpu().numpy()],sc__)))
         batch_sc_label[batch_sc_label_keys[sc_ind]] = np.array(sc_labels)
+
     return batch_sc_label
 
 def collect_batch_scores(in_nodes,g_batch,pos_edges,neg_edges,args):
@@ -82,15 +86,15 @@ def fetch_dataloader(adj, edges, args):
             sampler = dgl.dataloading.NeighborSampler([num_neighbors,num_neighbors,num_neighbors])
         elif args.dataset in ['elliptic_aug','yelpchi_rtr','yelpchi_aug','cora_ori','weibo','weibo_aug','cora_triple_sc_all','tfinance','tfinance_aug','elliptic','cora_no_anom','cora_anom']:
             sampler = dgl.dataloading.MultiLayerFullNeighborSampler(3)
-
+       
         neg_sampler = dgl.dataloading.negative_sampler.Uniform(1)
         sampler = dgl.dataloading.as_edge_prediction_sampler(sampler,negative_sampler=neg_sampler)
         edges=adj.edges('eid')
         batch_size = args.batch_size if args.batch_size > 0 else int(adj.number_of_edges())
         if args.device == 'cuda':
-            dataloader = dgl.dataloading.DataLoader(adj, edges, sampler, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=0, device=args.device)
+            dataloader = dgl.dataloading.DataLoader(adj, edges, sampler, batch_size=batch_size, shuffle=False, drop_last=True, num_workers=0, device=args.device)
         else:
-            dataloader = dgl.dataloading.DataLoader(adj, edges, sampler, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=6, device=args.device)
+            dataloader = dgl.dataloading.DataLoader(adj, edges, sampler, batch_size=batch_size, shuffle=False, drop_last=True, num_workers=6, device=args.device)
     elif args.batch_type == 'node':
         batch_size = args.batch_size if args.batch_size > 0 else int(adj.number_of_nodes()/100)
         #sampler = dgl.dataloading.SAINTSampler(mode='walk',budget=[int(batch_size/3),batch_size])
@@ -104,11 +108,14 @@ def fetch_dataloader(adj, edges, args):
 
 def get_edge_batch(loaded_input):
     in_nodes, sub_graph_pos, sub_graph_neg, block = loaded_input
+    #in_nodes, sub_graph_pos, block = loaded_input
     pos_edges = sub_graph_pos.edges()
     neg_edges = sub_graph_neg.edges()
+    #neg_edges = None
     pos_edges = torch.vstack((pos_edges[0],pos_edges[1])).T
     neg_edges = torch.vstack((neg_edges[0],neg_edges[1])).T
     last_batch_node = torch.max(neg_edges)
+    #last_batch_node = torch.max(pos_edges)
     g_batch = block
     return in_nodes, pos_edges, neg_edges, g_batch, last_batch_node
 
@@ -247,6 +254,76 @@ class anom_classifier():
                 accs.append(np.intersect1d(label,anom_preds).shape[0]/anom_preds.shape[0])
         return accs, anom_preds.shape[0]
 
+    def plot_anom_sc(self,sorted_errors,anom,ms_anoms_num,color,scale_name,args):
+        rankings_sc = np.zeros(sorted_errors.shape[0])
+        rankings_sc[np.intersect1d(sorted_errors,anom,return_indices=True)[1]] = 1
+        rankings_sc = rankings_sc.nonzero()[0]
+        rankings_sc = np.append(rankings_sc,np.full(ms_anoms_num-rankings_sc.shape[0],np.max(rankings_sc)))
+        plt.plot(np.arange(rankings_sc.shape[0]),rankings_sc,color=color)
+        fpath = f'vis/hit_at_k_model_wise/{args.dataset}/rankings/{scale_name}'
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)
+        fname = f'{fpath}/{args.model}.pkl'
+        with open(fname,'wb') as fout:
+            pkl.dump(rankings_sc,fout)
+
+    def plot_anom_perc(self,sorted_errors,anom,color,scale_name,args):
+
+        rankings_sc = np.zeros(sorted_errors.shape[0])
+        rankings_sc[np.intersect1d(sorted_errors,anom,return_indices=True)[1]] = 1
+        rankings_sc = rankings_sc.nonzero()[0]
+        anom_tot = sorted_errors.shape[0]
+        percs_sc = np.zeros(anom_tot)
+        for ind,ranking_ind in enumerate(range(rankings_sc.shape[0]-1)):
+            percs_sc[rankings_sc[ranking_ind]:rankings_sc[ranking_ind+1]] = ind/rankings_sc.shape[0]
+        percs_sc[rankings_sc[-1]:] = 1.
+        #import ipdb ; ipdb.set_trace()
+        plt.plot(np.arange(percs_sc.shape[0]),percs_sc,color=color)
+        #fpath = f'vis/perc_at_k/{args.dataset}/rankings/{scale_name}'
+        #if not os.path.exists(fpath):
+        #    os.makedirs(fpath)
+        #fname = f'{fpath}/{args.model}.pkl'
+        #with open(fname,'wb') as fout:
+        #    pkl.dump(rankings_sc,fout)
+
+    def plot_percentages(self,hit_rankings,sorted_errors,args,anoms,ms_anoms_num,sc):
+    
+        plt.figure()
+        anom_single,anom_sc1,anom_sc2,anom_sc3=anoms
+        self.plot_anom_perc(sorted_errors,anom_single,'cyan','single',args) ; plt.legend(['single']) ; plt.twinx()
+        self.plot_anom_perc(sorted_errors,anom_sc1,'red','scale1',args) ; plt.legend(['sc1']) ; plt.twinx()
+        self.plot_anom_perc(sorted_errors,anom_sc2,'blue','scale2',args) ; plt.legend(['sc2']) ;  plt.twinx()
+        self.plot_anom_perc(sorted_errors,anom_sc3,'purple','scale3',args) ; plt.legend(['sc3']) 
+        #plt.plot(np.arange(ms_anoms_num),hit_rankings,'gray')
+        #plt.legend(['single','sc1','sc2','sc3','total'])
+        fpath = f'vis/perc_at_k/{args.dataset}/{args.model}/{args.label_type}/{args.epoch}'
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)
+        plt.xlabel('# predictions')
+        plt.ylabel('% anomaly detected')
+        #plt.axhline(y=ms_anoms_num, color='b', linestyle='-')
+        #plt.ylim(top=ms_anoms_num*2)
+        plt.savefig(f'{fpath}/sc{sc}_perc_at_k.png')
+
+
+    def hit_at_k(self,hit_rankings,sorted_errors,args,anoms,ms_anoms_num,sc):
+        plt.figure()
+        anom_single,anom_sc1,anom_sc2,anom_sc3=anoms
+        self.plot_anom_sc(sorted_errors,anom_single,ms_anoms_num,'cyan','single',args)
+        self.plot_anom_sc(sorted_errors,anom_sc1,ms_anoms_num,'red','scale1',args)
+        self.plot_anom_sc(sorted_errors,anom_sc2,ms_anoms_num,'blue','scale2',args)
+        self.plot_anom_sc(sorted_errors,anom_sc3,ms_anoms_num,'purple','scale3',args)
+        plt.plot(np.arange(ms_anoms_num),hit_rankings,'gray')
+        plt.legend(['single','sc1','sc2','sc3','total'])
+        fpath = f'vis/hit_at_k/{args.dataset}/{args.model}/{args.label_type}/{args.epoch}'
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)
+        plt.ylabel('# predictions')
+        plt.xlabel('# multi-scale anomalies detected')
+        #plt.axhline(y=ms_anoms_num, color='b', linestyle='-')
+        #plt.ylim(top=ms_anoms_num*2)
+        plt.savefig(f'{fpath}/sc{sc}_hit_at_k.png')
+
     def calc_prec(self, graph, scores, label, sc_label, args, cluster=False, input_scores=False, clust_anom_mats=None, clust_inds=None):
         '''
         Input:
@@ -289,40 +366,13 @@ class anom_classifier():
 
             full_anoms = label.nonzero()[0]
             ms_anoms_num = full_anoms.shape[0]
-
-            def plot_anom_sc(sorted_errors,anom,ms_anoms_num,color,scale_name,args):
-                rankings_sc = np.zeros(sorted_errors.shape[0])
-                rankings_sc[np.intersect1d(sorted_errors,anom,return_indices=True)[1]] = 1
-                rankings_sc = rankings_sc.nonzero()[0]
-                rankings_sc = np.append(rankings_sc,np.full(ms_anoms_num-rankings_sc.shape[0],np.max(rankings_sc)))
-                plt.plot(np.arange(ms_anoms_num),rankings_sc,color=color)
-                fpath = f'vis/hit_at_k_model_wise/{args.dataset}/rankings/{scale_name}'
-                if not os.path.exists(fpath):
-                    os.makedirs(fpath)
-                fname = f'{fpath}/{args.model}.pkl'
-                with open(fname,'wb') as fout:
-                    pkl.dump(rankings_sc,fout)
             
             hit_rankings = rankings.nonzero()[0]
+            self.hit_at_k(hit_rankings,sorted_errors,args,[anom_single,anom_sc1,anom_sc2,anom_sc3],ms_anoms_num,sc)
+            self.plot_percentages(hit_rankings,sorted_errors,args,[anom_single,anom_sc1,anom_sc2,anom_sc3],ms_anoms_num,sc)
 
             #import ipdb ; ipdb.set_trace()
             # add plots for scale-specific anomalies
-
-            plt.figure()
-            plot_anom_sc(sorted_errors,anom_single,ms_anoms_num,'cyan','single',args)
-            plot_anom_sc(sorted_errors,anom_sc1,ms_anoms_num,'red','scale1',args)
-            plot_anom_sc(sorted_errors,anom_sc2,ms_anoms_num,'blue','scale2',args)
-            plot_anom_sc(sorted_errors,anom_sc3,ms_anoms_num,'purple','scale3',args)
-            plt.plot(np.arange(ms_anoms_num),hit_rankings,'gray')
-            plt.legend(['single','sc1','sc2','sc3','total'])
-            fpath = f'vis/hit_at_k/{args.dataset}/{args.model}/{args.label_type}/{args.epoch}'
-            if not os.path.exists(fpath):
-                os.makedirs(fpath)
-            plt.ylabel('# predictions')
-            plt.xlabel('# multi-scale anomalies detected')
-            #plt.axhline(y=ms_anoms_num, color='b', linestyle='-')
-            #plt.ylim(top=ms_anoms_num*2)
-            plt.savefig(f'{fpath}/sc{sc}_hit_at_k.png')
    
             print(f'SCALE {sc+1} loss',np.sum(node_scores),np.mean(node_scores))
 

msgad/visualization.py
@@ -4,7 +4,7 @@ from scipy.interpolate import make_interp_spline
 from model import *
 
 class Visualizer:
-    def __init__(self,adj,feats,args,sc_label,norms,anoms):
+    def __init__(self,adj,feats,args,sc_label,norms,anoms,recons_labels):
         self.device = args.device
         self.dataset = args.dataset
         self.model = args.model
@@ -15,16 +15,21 @@ class Visualizer:
         self.norms=norms
         self.anom=anoms
         self.sc_label=sc_label
+        self.recons_labels=recons_labels
         pass
 
-    def plot_spectrum(self,e,U,signal):
+    def plot_spectrum(self,e,U,signal,color=None):
+        #import ipdb ; ipdb.set_trace()
         c = U.T@signal
-        M = torch.zeros((21,c.shape[1])).to(e.device)
-        for j in range(e.shape[0]):
-            idx = min(int(e[j] / 0.1), 21-1)
+        M = torch.zeros((10,c.shape[1])).to(e.device).to(U.dtype)
+        for j in range(c.shape[0]):
+            idx = min(int(e[j] / 0.1), 10-1)
             M[idx] += c[j]**2
         M=M/sum(M)
-        y = M[:,0].detach().cpu().numpy()
+        #y = M[:,0].detach().cpu().numpy()
+        #print('nans',torch.where(torch.isnan(M))[0].shape)
+        M[torch.where(torch.isnan(M))]=0
+        y = torch.mean(M,axis=1).detach().cpu().numpy()
         x = np.arange(y.shape[0])
         try:
             spline = make_interp_spline(x, y)
@@ -33,7 +38,10 @@ class Visualizer:
         X_ = np.linspace(x.min(), x.max(), 500)
         Y_ = spline(X_)
         plt.xlabel('lambda')
-        plt.plot(X_,Y_)
+        if color:
+            plt.plot(X_,Y_,color=color)
+        else:
+            plt.plot(X_,Y_)
 
     def get_spectrum(self,mat):
         d_ = np.zeros(mat.shape[0])
@@ -43,18 +51,34 @@ class Visualizer:
         disconnected = (dw == 0)
         np.power(dw, -0.5, where=~disconnected, out=d_)
         D = scipy.sparse.diags(d_)
-        L = torch.eye(mat.shape[0])
-        L -= torch.tensor(D * mat.detach().cpu().numpy() * D)
+        #L = torch.eye(mat.shape[0])
+        mat_sparse = scipy.sparse.csr_matrix(mat.detach().cpu().numpy())
+        L = scipy.sparse.identity(mat.shape[0]) - D * mat_sparse * D
+        #L -= torch.tensor(D * mat.detach().cpu().numpy() * D)
         L[disconnected, disconnected] = 0
-        L = L.to(mat.device)
-        print('eig')
+        L.eliminate_zeros()
+        #L = L.to(mat.device)
+        '''
+        edges = torch.vstack((mat.edges()[0],mat.edges()[1])).T
+        L = torch_geometric.utils.get_laplacian(edges.T,torch.sigmoid(mat.edata['w']),normalization='sym',num_nodes=self.feats.shape[0])
+        L = torch_geometric.utils.to_dense_adj(L[0],edge_attr=L[1]).to(mat.device)[0]
+        #print('eig', torch.cuda.memory_allocated()/torch.cuda.max_memory_reserved())
+        '''
         try:
-            e,U = torch.linalg.eigh(L)
+            e,U = torch.linalg.eigh(torch.tensor(L.toarray()).to(mat.device))
+            #e,U=scipy.linalg.eigh(np.asfortranarray(L.toarray()), overwrite_a=True)
+            #e,U=scipy.linalg.eigh(np.asfortranarray(L.detach().cpu().numpy()), overwrite_a=True)
+            assert -1e-5 < e[0] < 1e-5
+            e[0] = 0
         except Exception as e:
             print(e)
+            import ipdb ; ipdb.set_trace()
             return
-        del L, D ; torch.cuda.empty_cache()
-        print('eig done')
+        e,U = torch.tensor(e).to(mat.device),torch.tensor(U).to(mat.device)
+        #del L, edges ; torch.cuda.empty_cache() ; gc.collect()
+        #import ipdb ; ipdb.set_trace()
+        del L, D ; torch.cuda.empty_cache() ; gc.collect()
+        #print('eig done', torch.cuda.memory_allocated()/torch.cuda.max_memory_reserved())
         '''
         try:
             assert -1e-5 < e[0] < 1e-5
@@ -63,7 +87,7 @@ class Visualizer:
             import ipdb ; ipdb.set_trace()
             e[0] = 0
         '''
-        return e, U.to(torch.float32)
+        return e, U#.to(torch.float32)
 
     def flatten_label(self,anoms):
         anom_flat = anoms[0][0]
@@ -74,17 +98,20 @@ class Visualizer:
     def plot_recons(self,recons_a):
         #import ipdb ; ipdb.set_trace()
         #recons_a = recons_a.detach().cpu().numpy()
-        plt.figure()
-        legend = ['og']
-        try:
-            e_adj,U_adj= self.get_spectrum(self.adj.adjacency_matrix().to_dense().to(self.device))
-        except Exception as e:
-            print(e)
-            import ipdb ; ipdb.set_trace()
-        e_adj,U_adj = e_adj.detach().cpu(),U_adj.detach().cpu()
-        self.plot_spectrum(e_adj,U_adj,self.feats[self.adj.dstnodes()])
-
+        #plt.figure()
+        #legend = ['og']
+        #e_adj,U_adj= self.get_spectrum(self.adj.adjacency_matrix().to_dense().to(self.device).to(torch.float64))
+        #e_adj,U_adj = e_adj.detach().cpu(),U_adj.detach().cpu()
+        #self.plot_spectrum(e_adj,U_adj,self.feats[self.adj.dstnodes()].to(U_adj.dtype))
         for r_ind,r_ in enumerate(recons_a):
+            # plot label
+            plt.figure()
+            e_adj,U_adj= self.get_spectrum(self.recons_labels[r_ind].to(self.device).to(torch.float64))
+            #e_adj,U_adj= self.get_spectrum(self.adj.adjacency_matrix().to_dense().to(self.device).to(torch.float64))
+            e_adj,U_adj = e_adj.detach().cpu(),U_adj.detach().cpu()
+            self.plot_spectrum(e_adj,U_adj,self.feats[self.adj.dstnodes()].to(U_adj.dtype))
+
+
             #r_ = torch.tensor(np.ceil(r_)).detach().to(self.device)
             #r_ = torch.sigmoid(torch.tensor(r_)).to(self.device)
             r_ = torch.tensor(r_).to(self.device)
@@ -97,19 +124,16 @@ class Visualizer:
 
             if len(torch.nonzero(r_symm))==0:
                 import ipdb ; ipdb.set_trace()
-            try:
-                e,U= self.get_spectrum(r_symm)
-            except Exception as e:
-                print(e)
-                import ipdb ; ipdb.set_trace()
+            e,U= self.get_spectrum(r_symm.to(torch.float64))
             e,U = e.detach().cpu(),U.detach().cpu()
-            self.plot_spectrum(e,U,self.feats[self.adj.dstnodes()])
-            legend.append(f'{r_ind}')
-        fpath = f'vis/recons_vis/{self.dataset}/{self.model}/{self.label_type}'
-        if not os.path.exists(fpath):
-            os.makedirs(fpath)
-        plt.legend(['original graph','sc1 recons.','sc2 recons.','sc3 recons.'])
-        plt.savefig(f'{fpath}/recons_vis_{self.epoch}_test.png')
+            self.plot_spectrum(e,U,self.feats[self.adj.dstnodes()].to(U.dtype))
+            #legend.append(f'{r_ind}')
+            legend = ['label','recons']
+            fpath = f'vis/recons_vis/{self.dataset}/{self.model}/{self.label_type}/{self.epoch}'
+            if not os.path.exists(fpath):
+                os.makedirs(fpath)
+            plt.legend(['original graph','sc1 recons.','sc2 recons.','sc3 recons.'])
+            plt.savefig(f'{fpath}/recons_vis_{r_ind}_test.png')
 
     def plot_filters(self,res_a_all):
         plt.figure()
@@ -119,8 +143,8 @@ class Visualizer:
         adj_label=torch.maximum(adj_label, adj_label.T).to(self.adj.device) 
         adj_label += torch.eye(adj_label.shape[0]).to(self.adj.device)
         try:
-            e_adj,U_adj = self.get_spectrum(adj_label)
-            self.plot_spectrum(e_adj.detach().cpu(),U_adj.detach().cpu(),self.feats[self.adj.dstnodes()])
+            e_adj,U_adj = self.get_spectrum(adj_label.to(torch.float64))
+            self.plot_spectrum(e_adj.detach().cpu(),U_adj.detach().cpu(),self.feats[self.adj.dstnodes()].to(U_adj.dtype))
         except Exception as e:
             print(e)
             import ipdb ; ipdb.set_trace()
@@ -129,7 +153,7 @@ class Visualizer:
         for res in res_a_all:
             res = torch.tensor(res).to(self.device).to(torch.float32)
             try:
-                self.plot_spectrum(e_adj,U_adj,res[self.adj.dstnodes()])
+                self.plot_spectrum(e_adj,U_adj,res[self.adj.dstnodes()].to(U_adj.dtype))
             except Exception as e:
                 print(e)
                 import ipdb ; ipdb.set_trace()
@@ -141,7 +165,7 @@ class Visualizer:
             os.makedirs(fpath)
         plt.savefig(f'{fpath}/filter_vis_{self.label_type}_{self.epoch}_test.png')
 
-    def plot_attn_scores(self,attn_weights):
+    def plot_attn_scores(self,attn_weights,lams):
         def flatten_label_attn(sc_label):
             anom_flat = sc_label[0]#[0]
             for i in sc_label[1:]:
@@ -170,11 +194,16 @@ class Visualizer:
         legend = []
         colors=['green','red','blue','purple','yellow']
         legend=['norm','anom sc1','anom sc2','anom sc3','single']
-
+        # scales x groups x filters x seq
+        scale_com_atts = np.zeros((3,len(legend),attn_weights.shape[2],attn_weights.shape[0]))
+        # for each scale
         for scale in range(attn_weights.shape[1]):
             p_min,p_max=np.inf,-np.inf
+            model_lams = lams[scale]
+            # for each filter
             for filter in range(attn_weights.shape[2]):
                 plt.figure()
+                # for each group
                 for ind,attn_weight in enumerate(attn_weights_arr):
                     data=attn_weight[:,scale,filter,:]
                     scale_attn = data.mean(axis=1)
@@ -184,12 +213,12 @@ class Visualizer:
                         p_max = np.max(scale_attn)
                     
                     plt.plot(scale_attn,color=colors[ind])
+                    scale_com_atts[scale,ind,filter]=scale_attn*model_lams[filter]
                     #try:
                     #    plt.fill_between(np.arange(data.shape[0]), data.mean(axis=1)[0] - data.std(axis=1)[0], data.mean(axis=1)[0] + data.std(axis=1)[0], color=colors[ind], alpha=0.1, label='_nolegend_')
                     #except Exception as e:
                     #    print(e)
                         #import ipdb ; ipdb.set_trace()
-            
                 plt.legend(legend)
                 plt.xlabel('epochs')
                 if self.sc_label is None:
@@ -202,3 +231,11 @@ class Visualizer:
                 plt.ylim((p_min,p_max))
                 #plt.xlim((args.epoch-3,args.epoch-1))
                 plt.savefig(f'{fpath}/model_sc{scale}_fil{filter}.png')
+        for sc,com_att in enumerate(scale_com_atts):
+            plt.figure()
+            # group : filters x seq
+            for ind,group in enumerate(com_att):
+                plt.plot(group.sum(axis=0),color=colors[ind])
+            plt.legend(legend)
+            plt.savefig(f'{fpath}/combined_filters_scale{sc}.png')
+            